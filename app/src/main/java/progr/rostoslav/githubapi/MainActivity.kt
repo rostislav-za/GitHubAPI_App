7c7f77a02ec2fed10753c3ff67b5012807a4c99f
Rosstislav <randomadman@gmail.com>
2020-03-22 15:38:06 +0400

    added navigation

--- app/src/main/java/progr/rostoslav/githubapi/MainActivity.kt
+++ app/src/main/java/progr/rostoslav/githubapi/MainActivity.kt
@@ -2,11 +2,25 @@ package progr.rostoslav.githubapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
+    }
+    private fun initNavigation() {
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navFragment) as NavHostFragment? ?: return
+        navController = host.navController
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomBar?.setupWithNavController(navController)
     }
 }
